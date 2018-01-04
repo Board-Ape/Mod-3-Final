74bffa00b82aec2fbaf2335cedbd6a721e695bf7
Sam Singer <samuel.singer1@gmail.com>
2018-01-04 14:11:50 -0700

    Forgot To Add To Root Reducer

--- src/reducers/root-reducer.js
+++ src/reducers/root-reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { fake } from './fake-reducer';
 import houseData from './houseData-reducer';
 import members from './members-reducer';
+import currentView from './currentView-reducer';
 
 const rootReducer = combineReducers({
   fake,
   houseData,
-  members
+  members,
+  currentView
 });
 
 export default rootReducer;
