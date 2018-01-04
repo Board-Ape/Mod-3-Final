8811da4eb6dc91f61f9c30dc459ebde3763694c2
Sam Singer <samuel.singer1@gmail.com>
2018-01-04 15:41:47 -0700

    Reducers Test Complete

--- src/reducers/Reducers.test.js
+++ src/reducers/Reducers.test.js
@@ -1,7 +1,9 @@
 import houseData from './houseData-reducer';
+import members from './members-reducer';
+import currentView from './currentView-reducer';
 import * as actions from '../actions'
 
-describe('All Reducers', () => {
+describe('houseData reducer', () => {
 
   // HouseData Reducer
 
@@ -15,8 +17,42 @@ describe('All Reducers', () => {
     const mockHousesObject = {gramps: 'grump', gum: 'bum'};
     const expected = mockHousesObject;
 
-    expect(houseData(
-      {}, actions.fetchHousesSuccess(mockHousesObject))).toEqual(expected);
+    expect(houseData({}, actions.fetchHousesSuccess(mockHousesObject))).toEqual(expected);
+  });
+
+  // Members Reducer
+
+  it('should have an initial state', () => {
+    const expectedState = [];
+
+    expect(members(undefined, [])).toEqual(expectedState);
+  });
+
+  it('should return a new state with an action', () => {
+    const houseMembers = {aloy: 'Metal', prim: 'Rose'};
+    const expected = houseMembers;
+
+    expect(members({}, actions.fetchMembersSuccess(houseMembers))).toEqual(expected);
+  });
+
+  // CurrentView reducer
+
+  it('should have an initial state', () => {
+    const expectedState = 'houses';
+
+    expect(currentView(undefined, [])).toEqual(expectedState);
+  });
+
+  it('should return a new state with an action', () => {
+    const mockHouses = 'houses';
+    const mockMembers = 'members';
+    const expectedHouses = mockHouses;
+    const expectedMembers = mockMembers;
+    const expectedReset = mockHouses;
+
+    expect(currentView({}, actions.fetchHousesSuccess(mockHouses))).toEqual(expectedHouses);
+    expect(currentView({}, actions.fetchMembersSuccess(mockMembers))).toEqual(expectedMembers);
+    expect(currentView({}, actions.resetViewToHouses(mockHouses))).toEqual(expectedReset);
   });
 
 });
